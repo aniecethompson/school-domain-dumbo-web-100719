d25bc9709f452704b1ce27107ce07db0cd64a984
Aniece Thompson <aniecet@yahoo.com>
2019-10-16 21:13:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,25 @@
-require 'pry'
-
 class School
-  attr_reader :name, :roster
-  
+  attr_accessor :name, :roster, :student, :grade
+ 
   def initialize(name)
-  @name = name
-  @roster = {}
+    @name=name
+    @roster = {}
+    # @student= student
+    # @grade= grade
   end 
   
   def add_student(student, grade)
-  @roster[grade] ||= []
-  @roster[grade] << student
+    @roster[grade] ||= []
+    @roster[grade] << student
   end
   
   def grade(grade)
-  @roster[grade]  
+    @roster[grade]
   end
-  
+    
   def sort
   @roster.each do |grade, student|
   @roster[grade].sort!
   end
   end
-    
-
-  
 end
